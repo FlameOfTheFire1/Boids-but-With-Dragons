8bc9ce8e03cccb9bd73bec225f4a590691c7bd2d
FlameOfTheFire1 <124635735+FlameOfTheFire1@users.noreply.github.com>
2023-04-23 17:34:13 -0700

    strategy

---
+++ spritesheet.js
@@ -0,0 +1,14 @@
+class spriteSheet{
+    images = []; //imageLoops
+    hidden; 
+
+    constructor(fileName){
+        this.source = fileName;
+    }
+
+    getImage(rectangle){
+        //rectangle gives the size of the sprite image
+
+        return image;
+    }
+}
